a1a3e881e1d2392680154698c27b6995bd1c7bf2
Margaretha Juan <tulip_holy@yahoo.com>
2017-11-02 01:21:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,17 +16,21 @@ WIN_COMBINATIONS = [
 ]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |combination|
-    position_1 = board[combination[0]]
-    position_2 = board[combination[1]]
-    position_3 = board[combination[2]]
+  if !board.empty?
+    WIN_COMBINATIONS.each do |combination|
+      position_1 = board[combination[0]]
+      position_2 = board[combination[1]]
+      position_3 = board[combination[2]]
 
-    if ((position_1 == "X" && position_2 == "X" && position_3 == "X") ||
-      (position_1 == "O" && position_2 == "O" && position_3 == "O"))
-      return combination
-    else
-      false
+      if ((position_1 == "X" && position_2 == "X" && position_3 == "X") ||
+        (position_1 == "O" && position_2 == "O" && position_3 == "O"))
+        return combination
+      else
+        false
+      end
     end
+  else
+    false
   end
 end
 
